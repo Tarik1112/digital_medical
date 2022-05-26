43174aa39676d1e70e4f8bb0ae916574811bb78d
Tarik1112 <tare.mujke@gmail.com>
2022-05-26 21:29:06 +0200

    Setting up the project

---
+++ server/src/main/java/com/digital_medical/DigitalMedical/DigitalMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.digital_medical.DigitalMedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMedicalApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/digital_medical/DigitalMedical/DigitalMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_medical.DigitalMedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
